e1f24952b4634d5934f73a471249132357f27b0b
wakame009 <sayuri@sayurinombp.elecom>
2023-11-11 23:45:55 +0900

    kadai-java-review01

---
+++ src/Review01.java
@@ -0,0 +1,11 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        int price = 1500;
+        int tax = (int)(price * 0.1);
+        int pay = price + tax;
+        System.out.println(price + "円の商品の税込み価格は" + pay + "円(消費税は" + tax + "円)です。");
+    }
+
+}
