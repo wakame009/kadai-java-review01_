0c517face00805de554a98f3b69fbba3e8a16a55
wakame009 <sayuri@sayurinombp.elecom>
2023-11-12 22:05:03 +0900

    kadai-java-review01

--- src/Review01.java
+++ src/Review01.java
@@ -2,11 +2,19 @@
 public class Review01 {
 
     public static void main(String[] args) {
+        // 商品価格の初期値設定
         int price = 1500;
-        int tax = (int)(price * 0.1);
-        int pay = price + tax;
-        System.out.println(price + "円の商品の税込み価格は" + pay + "円(消費税は" + tax + "円)です。");
+       
+        // taxメソッドから税のみを出す
+        int taxOnly = tax(price);
+
+        // 税込み価格を出す
+        int includeTax = price + taxOnly;
+        
+        // 出力
+        System.out.println(price + "円の商品の税込み価格は" + includeTax + "円(消費税は" + taxOnly + "円)です。");
     }
+    
     public static int tax(int price) {
         int result = (int)(price * 0.1);
         return result;
